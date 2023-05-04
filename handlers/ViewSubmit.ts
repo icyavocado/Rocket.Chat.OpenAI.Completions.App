231d0980dd1543dec44e7c63b1a83146df028625
Dai <dainsait@gmail.com>
2023-05-04 13:17:30 -0600

    UPDATE formated the message with Answer header - to improve visibility, and separate the prompt from the answer

--- handlers/ViewSubmit.ts
+++ handlers/ViewSubmit.ts
@@ -100,7 +100,7 @@ export class ViewSubmitHandler {
                                 // remove initial break lines
                                 content = content.replace(/^\s*/gm, "");
                                 var before_message = `**Instruction**: ${instruction}\n**Prompt**: ${prompt}`;
-                                var message = before_message + "\n" + content;
+                                var message = before_message + "\n**Answer:** " + content;
 
                                     switch (output_mode) {
                                         case "notification":
